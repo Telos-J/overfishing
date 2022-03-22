debcf041568aebd18e3718b1bdd3edd99501a2a9
kwonth <kwon219005@gmail.com>
2022-03-22 21:58:26 +0900

    made triangle

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ console.log('Hello World')
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
-let x = 10
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let x = 500
 
 
 function gameLoop() {
-    x += 6
-    if (x >= canvas.width) {
-        x = -30
-    } 
-    context.fillStyle = 'white'
+    // x += 6
+    // if (x >= canvas.width) {
+    //     x = -30
+    // } 
+    context.fillStyle = '#cefffe'
     context.fillRect(0, 0, canvas.width, canvas.height)
-    context.fillStyle = 'black'
-    context.fillRect(x, 90, 30, 30)
+    context.fillStyle = '#8cbe87'
+    context.beginPath()
+    context.moveTo(x, 90)
+    context.lineTo(x-100, 90-30)
+    context.lineTo(x-100, 90+60)
+    context.fill()
     requestAnimationFrame(gameLoop)
 }
 
